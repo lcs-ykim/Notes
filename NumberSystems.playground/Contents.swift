6065164110070d4d5d968ae81d37c2bf87a7246d
Yeseo Kim <yeseokim4@gmail.com>
2021-01-08 12:26:24 -0500

    Example of converting from decimal to binary

---
+++ NumberSystems.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+//Find the digits that represent 27 in binary
+//We know this is 11011 in base 2
+
+//Use the remainder operator to get the first binary digit
+//The remainder operator is %
+27 % 2
+
+//Find the decimal value left to convert
+//The division operator is /
+27 / 2
+
+//Get next binary digit
+13 % 2
+//Get value left to convert
+13 / 2
+
+//Next digit
+6 % 2
+//Value left
+6 / 2
+
+//Next digit
+3 % 2
+//Value left
+2 / 2
+
+//Last binary digit
+1 % 2
+//Done - because decimal value left is 0
+1 / 2
